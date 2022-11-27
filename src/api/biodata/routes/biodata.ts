05c636d3d25c5b5bcd17a1f45cdb9424c3d50a68
Gifaldy Azka <me@falcxxdev.ml>
2022-11-27 19:06:49 +0700

    feat: initial commit

---
+++ src/api/biodata/routes/biodata.ts
@@ -0,0 +1,7 @@
+/**
+ * biodata router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::biodata.biodata');
---
+++ src/api/biodata/services/biodata.ts
@@ -0,0 +1,7 @@
+/**
+ * biodata service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::biodata.biodata');

7b6e8507836ca9a29b8bb22dd33570d03caa6b54
Gifaldy Azka <me@falcxxdev.ml>
2022-11-29 18:38:25 +0700

    feat: add friendship content-type

---
+++ src/api/friendship/controllers/friendship.ts
@@ -0,0 +1,7 @@
+/**
+ * friendship controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::friendship.friendship');
---
+++ src/api/friendship/services/friendship.ts
@@ -0,0 +1,7 @@
+/**
+ * friendship service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::friendship.friendship');
